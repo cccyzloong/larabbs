8b335960ae934e4e8f2fc9544c3e05e2d416bee6
cccyzloong <cccyzloong@163.com>
2019-07-31 16:27:01 +0800

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
     public function update(UserRequest $request, User $user){
+        //dd($request->all());
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
