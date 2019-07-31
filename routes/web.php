3ba20e28104fb1eb36397a2d33da73de110ce651
cccyzloong <cccyzloong@163.com>
2019-07-31 11:40:28 +0800

    注册、验证码

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,10 @@
 */
 
 /* Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo phpinfo();
 }); */
+
 Route::get('/','PagesController@root')->name('root');
 
 //Auth::routes();
