7bff0949f4e5c4552d01407474345e7fba122ce2
cccyzloong <cccyzloong@163.com>
2019-08-01 11:08:22 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 //个人中心页面
 Route::resource('users', 'UsersController')->only(['edit','update','show']);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
