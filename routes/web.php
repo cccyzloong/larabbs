c5ff297a1061b9fefce2bb1795af2cfd815ffc12
cccyzloong <cccyzloong@163.com>
2019-08-01 14:34:34 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,4 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users', 'UsersController')->only(['edit','update','show']);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
